8891386b85661c45d1034b2719887c9ea9314986
Herman <contactliuhao@gmail.com>
2020-03-27 16:44:49 +0800

    update favicon and some configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,42 @@
 module.exports = {
   plugins: [
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-theme-blog`,
       options: {},
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Coderlog`,
+        short_name: `Coderlog`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#007acc`,
+        display: `minimal-ui`,
+        icon: `src/assets/favicon.png`,
+        theme_color_in_head: false,
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `Coderlog`,
+    author: `Herman`,
+    description: `Personal blog by Herman. I explain with words and code.`,
+    siteUrl: "https://liuhao.netlify.com/",
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/beliuhao`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/beliuhao`,
       },
     ],
   },
--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -7,10 +7,9 @@ import { Styled } from "theme-ui"
 
 export default () => (
   <Fragment>
-    This is where <Styled.a href="http://example.com/">your name</Styled.a>
-    {` `}
-    goes.
-    <br />
-    Or whatever, you make the rules.
+    Personal blog by{" "}
+    <Styled.a href="https://liuhao.netlify.com/">Herman</Styled.a>
+    {` `}.
+    <br />I explain with words and code.
   </Fragment>
 )
