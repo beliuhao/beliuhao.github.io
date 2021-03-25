b3c04fa6a2a3b06d3c9279a5a36c41d11fc79281
Herman <contactliuhao@gmail.com>
2021-03-26 01:03:21 +0800

    feat(reverse-notes-list): shadow the file-list of gatsby-theme-notes for reversed display order

---
+++ content/posts/2021-03-26-hello-gatsby.md
@@ -0,0 +1,7 @@
+---
+title: Hello Gatsby
+date: 2021-03-26
+author: Herman
+---
+
+Hello! Gatsby.
---
+++ src/gatsby-theme-notes/components/file-list.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "gatsby"
+import { Styled } from "theme-ui"
+
+export default ({ files }) => (
+  <ul css={{ padding: 0 }}>
+    {files
+      .map((url) => (
+        <li key={url}>
+          <Styled.a as={Link} to={url}>
+            {url}
+          </Styled.a>
+        </li>
+      ))
+      .reverse()}
+  </ul>
+)
