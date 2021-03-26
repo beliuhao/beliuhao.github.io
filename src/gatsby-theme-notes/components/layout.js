2bff80300b57b247b44d93a556443d390296e84a
Herman <contactliuhao@gmail.com>
2021-03-26 14:08:59 +0800

    update(content): change some description words

--- src/gatsby-theme-notes/components/layout.js
+++ src/gatsby-theme-notes/components/layout.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { css, Styled } from "theme-ui"
 import BlogLayout from "gatsby-theme-blog/src/components/layout"
 import Bio from "gatsby-theme-blog/src/components/bio"
 
@@ -11,11 +10,6 @@ export default (props) => (
         marginTop: `120px`,
       }}
     >
-      <Styled.hr
-        css={css({
-          mt: -3,
-        })}
-      />
       <Bio />
     </div>
   </BlogLayout>
