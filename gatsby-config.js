adcf4287d6aa7d0731bb88a81224b88933a36e1b
Herman <contactliuhao@gmail.com>
2021-03-29 16:50:06 +0800

    feat(add-rss-feed): add rss feed for automatic post links update on github profile homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,21 @@
 module.exports = {
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Hao Liu`,
+    author: `@beliuhao`,
+    description: `Personal blog by Herman. I build things with words and codes.`,
+    siteUrl: "https://beliuhao.github.io/",
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/beliuhao`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/beliuhao`,
+      },
+    ],
+  },
   plugins: [
     `gatsby-plugin-sharp`,
     `@pauliescanlon/gatsby-mdx-embed`,
@@ -33,22 +50,63 @@ module.exports = {
         crossOrigin: `use-credentials`,
       },
     },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allBlogPost } }) => {
+              return allBlogPost.edges.map((edge) => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.date,
+                  url: site.siteMetadata.siteUrl + edge.node.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.slug,
+                  title: edge.node.title,
+                })
+              })
+            },
+            query: `
+              {
+                allBlogPost(
+                  sort: { order: ASC, fields: date },
+                ) {
+                  edges {
+                    node {
+                      id
+                      slug
+                      excerpt
+                      date
+                      title
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Herman Blog's RSS Feed",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            // match: "^/blog/",
+            // optional configuration to specify external rss feed, such as feedburner
+            link: "https://feeds.feedburner.com/gatsby/blog",
+          },
+        ],
+      },
+    },
   ],
-  // Customize your site metadata:
-  siteMetadata: {
-    title: `Hao Liu`,
-    author: `Herman`,
-    description: `Personal blog by Herman. I build things with words and codes.`,
-    siteUrl: "https://beliuhao.github.io/",
-    social: [
-      {
-        name: `twitter`,
-        url: `https://twitter.com/beliuhao`,
-      },
-      {
-        name: `github`,
-        url: `https://github.com/beliuhao`,
-      },
-    ],
-  },
 }
