3ca0c69c04f57e086b54d51eef3bc3b47f4cb17c
Herman <contactliuhao@gmail.com>
2021-03-29 22:21:29 +0800

    update(bio-content): rename 'things' to 'stuff'.

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -8,7 +8,7 @@ import { Styled } from "theme-ui"
 export default () => (
   <Fragment>
     <Styled.a href="https://github.com/beliuhao/">I AKA Herman,</Styled.a>
-    {` `} build things with words and codes.
+    {` `} build stuff with words and codes.
     <br />『 不用闪躲，为我喜欢的生活而活。』
     <br />
     年少轻狂，幸福时光；老当益壮，剑拔弩张。
